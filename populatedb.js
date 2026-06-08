b4af55b7e2803139307ff2093a2963c806f93fd0
IsabelleSanchez <dasilvasanchezisabelle@gmail.com>
2026-06-08 10:46:13 -0300

    Versão completa

--- populatedb.js
+++ populatedb.js
@@ -1,48 +1,63 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
 
-/**
- * This script populates some test books, authors, genres, and book instances.
- */
+console.log(
+  'This script populates some test books, authors, genres and bookinstances to your database. Specified database as argument - e.g.: node populatedb "mongodb+srv://cooluser:coolpassword@cluster0.cojoign.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0"'
+);
 
 // Get arguments passed on command line
 const userArgs = process.argv.slice(2);
 
-const mongoose = require("mongoose");
-const Author = require("./models/author");
 const Book = require("./models/book");
-const BookInstance = require("./models/bookinstance");
+const Author = require("./models/author");
 const Genre = require("./models/genre");
+const BookInstance = require("./models/bookinstance");
 
 const genres = [];
 const authors = [];
 const books = [];
 const bookinstances = [];
 
-const mongoose_uri = userArgs[0];
+const mongoose = require("mongoose");
 
-mongoose.connect(mongoose_uri);
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+const mongoDB = userArgs[0];
 
-async function genreCreate(name) {
-  const genre = new Genre({ name });
+main().catch((err) => console.log(err));
+
+async function main() {
+  console.log("Debug: About to connect");
+  await mongoose.connect(mongoDB);
+  console.log("Debug: Should be connected?");
+  await createGenres();
+  await createAuthors();
+  await createBooks();
+  await createBookInstances();
+  console.log("Debug: Closing mongoose");
+  await mongoose.connection.close();
+}
+
+// We pass the index to the ...Create functions so that, for example,
+// genre[0] will always be the Fantasy genre, regardless of the order
+// in which the elements of promise.all's argument complete.
+async function genreCreate(index, name) {
+  const genre = new Genre({ name: name });
   await genre.save();
-  genres.push(genre);
+  genres[index] = genre;
   console.log(`Added genre: ${name}`);
 }
 
-async function authorCreate(first_name, family_name, d_birth, d_death) {
-  const authordetail = { first_name, family_name };
+async function authorCreate(index, first_name, family_name, d_birth, d_death) {
+  const authordetail = { first_name: first_name, family_name: family_name };
   if (d_birth != false) authordetail.date_of_birth = d_birth;
   if (d_death != false) authordetail.date_of_death = d_death;
 
   const author = new Author(authordetail);
+
   await author.save();
-  authors.push(author);
+  authors[index] = author;
   console.log(`Added author: ${first_name} ${family_name}`);
 }
 
-async function bookCreate(title, summary, isbn, author, genre) {
+async function bookCreate(index, title, summary, isbn, author, genre) {
   const bookdetail = {
     title,
     summary,
@@ -53,11 +68,11 @@ async function bookCreate(title, summary, isbn, author, genre) {
 
   const book = new Book(bookdetail);
   await book.save();
-  books.push(book);
+  books[index] = book;
   console.log(`Added book: ${title}`);
 }
 
-async function bookinstanceCreate(book, imprint, due_back, status) {
+async function bookInstanceCreate(index, book, imprint, due_back, status) {
   const bookinstancedetail = {
     book,
     imprint,
@@ -67,35 +82,35 @@ async function bookinstanceCreate(book, imprint, due_back, status) {
 
   const bookinstance = new BookInstance(bookinstancedetail);
   await bookinstance.save();
-  bookinstances.push(bookinstance);
+  bookinstances[index] = bookinstance;
   console.log(`Added bookinstance: ${imprint}`);
 }
 
 async function createGenres() {
   console.log("Adding genres");
   await Promise.all([
-    genreCreate("Fantasy"),
-    genreCreate("Science Fiction"),
-    genreCreate("French Poetry"),
-    genreCreate("Drama"),
+    genreCreate(0, "Fantasy"),
+    genreCreate(1, "Science Fiction"),
+    genreCreate(2, "French Poetry"),
   ]);
 }
 
 async function createAuthors() {
   console.log("Adding authors");
   await Promise.all([
-    authorCreate("Patrick", "Rothfuss", "1973-06-06", false),
-    authorCreate("Ben", "Bova", "1932-11-8", false),
-    authorCreate("Isaac", "Asimov", "1920-01-02", "1992-04-06"),
-    authorCreate("Bob", "Dylan", "1941-05-24", false),
-    authorCreate("Bill", "Bryson", "1951-12-08", false),
+    authorCreate(0, "Patrick", "Rothfuss", "1973-06-06", false),
+    authorCreate(1, "Ben", "Bova", "1932-11-8", false),
+    authorCreate(2, "Isaac", "Asimov", "1920-01-02", "1992-04-06"),
+    authorCreate(3, "Bob", "Billings", false, false),
+    authorCreate(4, "Jim", "Jones", "1971-12-16", false),
   ]);
 }
 
 async function createBooks() {
-  console.log("Adding books");
+  console.log("Adding Books");
   await Promise.all([
     bookCreate(
+      0,
       "The Name of the Wind (The Kingkiller Chronicle, #1)",
       "I have stolen princesses back from sleeping barrow kings. I burned down the town of Trebon. I have spent the night with Felurian and left with both my sanity and my life. I was expelled from the University at a younger age than most people are allowed in. I tread paths by moonlight that others fear to speak of during day. I have talked to Gods, loved women, and written songs that make the minstrels weep.",
       "9781473211896",
@@ -103,62 +118,111 @@ async function createBooks() {
       [genres[0]]
     ),
     bookCreate(
+      1,
       "The Wise Man's Fear (The Kingkiller Chronicle, #2)",
-      "Picking up the tale of Kvothe Arliden's son, from the end of The Name of the Wind, we find him in innkeeper, named Kote, who played Lucan Lakkey's sale of ears and okay. Cirka and Tempi following the Lethani...",
-      "9788401352023",
+      "Picking up the tale of Kvothe Kingkiller once again, we follow him into exile, into political intrigue, courtship, adventure, love and magic... and further along the path that has turned Kvothe, the mightiest magician of his age, a legend in his own time, into Kote, the unassuming pub landlord.",
+      "9788401352836",
       authors[0],
       [genres[0]]
     ),
     bookCreate(
+      2,
       "The Slow Regard of Silent Things (Kingkiller Chronicle)",
-      "Deep below the University, there is a dark matter. It had been imprisoned, until a series of circumstances -- starting with two human words -- set it free. And that truth of the driving knowledge of Elodin, the Master Namer, there is a powerful, dangerous thing. The Slow Regard of Silent Things is the story of the subtle energy of the world, the knowledge of names. Auri is Elodin's student, and The Slow Regard of the Silent Things is her story...",
-      "9780756411268",
+      "Deep below the University, there is a dark place. Few people know of it: a broken web of ancient passageways and abandoned rooms. A young woman lives there, tucked among the sprawling tunnels of the Underthing, snug in the heart of this forgotten place.",
+      "9780756411336",
       authors[0],
       [genres[0]]
     ),
     bookCreate(
-      "Eon (Revelation Space)",
-      "The first science fiction novel in Alastair reynold's epic space opera sequence. Humankind has finally encountered alien life, but the aliens, the Inhibitors, have fought interstellar wars on a scale that makes previous conflicts look like scuffles. They are spacefarers and voiders...",
-      "978057508927",
+      3,
+      "Apes and Angels",
+      "Humankind headed out to the stars not for conquest, nor exploration, nor even for curiosity. Humans went to the stars in a desperate crusade to save intelligent life wherever they found it. A wave of death is spreading through the Milky Way galaxy, an expanding sphere of lethal gamma ...",
+      "9780765379528",
       authors[1],
       [genres[1]]
+    ),
+    bookCreate(
+      4,
+      "Death Wave",
+      "In Ben Bova's previous novel New Earth, Jordan Kell led the first human mission beyond the solar system. They discovered the ruins of an ancient alien civilization. But one alien AI survived, and it revealed to Jordan Kell that an explosion in the black hole at the heart of the Milky Way galaxy has created a wave of deadly radiation, expanding out from the core toward Earth. Unless the human race acts to save itself, all life on Earth will be wiped out...",
+      "9780765379504",
+      authors[1],
+      [genres[1]]
+    ),
+    bookCreate(
+      5,
+      "Test Book 1",
+      "Summary of test book 1",
+      "ISBN111111",
+      authors[4],
+      [genres[0], genres[1]]
+    ),
+    bookCreate(
+      6,
+      "Test Book 2",
+      "Summary of test book 2",
+      "ISBN222222",
+      authors[4],
+      false
     ),
   ]);
 }
 
 async function createBookInstances() {
-  console.log("Adding book instances");
+  console.log("Adding authors");
   await Promise.all([
-    bookinstanceCreate(books[0], "London Gollancz, 2014.", false, "Available"),
-    bookinstanceCreate(
-      books[1],
-      "Gollancz, 2011.",
-      new Date("2024-12-31"),
+    bookInstanceCreate(
+      0,
+      books[0],
+      "London Gollancz, 2014.",
+      false,
+      "Available"
+    ),
+    bookInstanceCreate(1, books[1], " Gollancz, 2011.", false, "Loaned"),
+    bookInstanceCreate(2, books[2], " Gollancz, 2015.", false, false),
+    bookInstanceCreate(
+      3,
+      books[3],
+      "New York Tom Doherty Associates, 2016.",
+      false,
+      "Available"
+    ),
+    bookInstanceCreate(
+      4,
+      books[3],
+      "New York Tom Doherty Associates, 2016.",
+      false,
+      "Available"
+    ),
+    bookInstanceCreate(
+      5,
+      books[3],
+      "New York Tom Doherty Associates, 2016.",
+      false,
+      "Available"
+    ),
+    bookInstanceCreate(
+      6,
+      books[4],
+      "New York, NY Tom Doherty Associates, LLC, 2015.",
+      false,
+      "Available"
+    ),
+    bookInstanceCreate(
+      7,
+      books[4],
+      "New York, NY Tom Doherty Associates, LLC, 2015.",
+      false,
+      "Maintenance"
+    ),
+    bookInstanceCreate(
+      8,
+      books[4],
+      "New York, NY Tom Doherty Associates, LLC, 2015.",
+      false,
       "Loaned"
     ),
-    bookinstanceCreate(books[2], "Gollancz, 2015.", false, false),
-    bookinstanceCreate(
-      books[3],
-      "Gollancz SF, 1997.",
-      false,
-      "Reserved"
-    ),
+    bookInstanceCreate(9, books[0], "Imprint XXX2", false, false),
+    bookInstanceCreate(10, books[1], "Imprint XXX3", false, false),
   ]);
 }
-
-async function populate() {
-  await createGenres();
-  await createAuthors();
-  await createBooks();
-  await createBookInstances();
-}
-
-populate()
-  .catch((err) => {
-    console.log("POPULATION FAILED with error: " + err);
-    process.exit(1);
-  })
-  .then(() => {
-    console.log("POPULATION COMPLETE");
-    mongoose.connection.close();
-  });
--- routes/catalog.js
+++ routes/catalog.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-// Require controller modules.
+// Require our controllers.
 const book_controller = require("../controllers/bookController");
 const author_controller = require("../controllers/authorController");
 const genre_controller = require("../controllers/genreController");
@@ -34,7 +34,7 @@ router.post("/book/:id/update", book_controller.book_update_post);
 // GET request for one Book.
 router.get("/book/:id", book_controller.book_detail);
 
-// GET request for list of all Book items.
+// GET request for list of all Book.
 router.get("/books", book_controller.book_list);
 
 /// AUTHOR ROUTES ///
@@ -48,7 +48,7 @@ router.post("/author/create", author_controller.author_create_post);
 // GET request to delete Author.
 router.get("/author/:id/delete", author_controller.author_delete_get);
 
-// POST request to delete Author.
+// POST request to delete Author
 router.post("/author/:id/delete", author_controller.author_delete_post);
 
 // GET request to update Author.
@@ -94,37 +94,37 @@ router.get("/genres", genre_controller.genre_list);
 // GET request for creating a BookInstance. NOTE This must come before route that displays BookInstance (uses id).
 router.get(
   "/bookinstance/create",
-  book_instance_controller.bookinstance_create_get,
+  book_instance_controller.bookinstance_create_get
 );
 
 // POST request for creating BookInstance.
 router.post(
   "/bookinstance/create",
-  book_instance_controller.bookinstance_create_post,
+  book_instance_controller.bookinstance_create_post
 );
 
 // GET request to delete BookInstance.
 router.get(
   "/bookinstance/:id/delete",
-  book_instance_controller.bookinstance_delete_get,
+  book_instance_controller.bookinstance_delete_get
 );
 
 // POST request to delete BookInstance.
 router.post(
   "/bookinstance/:id/delete",
-  book_instance_controller.bookinstance_delete_post,
+  book_instance_controller.bookinstance_delete_post
 );
 
 // GET request to update BookInstance.
 router.get(
   "/bookinstance/:id/update",
-  book_instance_controller.bookinstance_update_get,
+  book_instance_controller.bookinstance_update_get
 );
 
 // POST request to update BookInstance.
 router.post(
   "/bookinstance/:id/update",
-  book_instance_controller.bookinstance_update_post,
+  book_instance_controller.bookinstance_update_post
 );
 
 // GET request for one BookInstance.
@@ -132,9 +132,5 @@ router.get("/bookinstance/:id", book_instance_controller.bookinstance_detail);
 
 // GET request for list of all BookInstance.
 router.get("/bookinstances", book_instance_controller.bookinstance_list);
-
-// GET catalog home page.
-router.get("/", book_controller.index); 
-// This actually maps to /catalog/ because we import the route with a /catalog prefix
 
 module.exports = router;
